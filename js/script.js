a19f3aabd353f22ee551e92e674a51e1b9cb26ce
anticni <n.antic91@gmail.com>
2017-12-11 12:18:58 +0100

    added date decoding function

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ function check(){
 		mm = str.slice(2,4),
 		yyy = str.slice(4,7),
 		rr = str.slice(7,9),
-		bbb = str.slice(9,11),
-		k = str.slice(11,13);
+		bbb = str.slice(9,12),
+		k = str.slice(12,13);
 
 console.log(dd);
 console.log(mm);
@@ -16,6 +16,26 @@ console.log(yyy);
 console.log(rr);
 console.log(bbb);
 console.log(k);
+
+var day = "Dan rodjenja: " + dd +".";
+var dayP = document.createElement("p");
+dayP.textContent = day;
+document.getElementById("data").appendChild(dayP);
+
+var month = "Mesec rodjenja: " + mm +".";
+var monthP = document.createElement("p");
+monthP.textContent = month;
+document.getElementById("data").appendChild(monthP);
+
+var year = "Godina rodjenja: 1" + yyy +".";
+var yearP = document.createElement("p");
+yearP.textContent = year;
+document.getElementById("data").appendChild(yearP);
+
+
+
+
+
 
 
 }
